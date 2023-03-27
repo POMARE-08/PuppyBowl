05cd8daf86e21f7858c7352d5781a78f2d469c31
Gyno Pomare <pomare-08@sandiego.edu>
2023-03-26 17:35:41 -0700

    start building project

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -1,15 +1,30 @@
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'COHORT-NAME';
+const cohortName = '2301-ftb-et-web-pt';
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
 
 export const fetchAllPlayers = async () => {
-
+  try {
+    const response = await fetch(`${APIURL}players`);
+    const result = await response.json();
+    // console.log(result);
+    return result.data.players
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const fetchSinglePlayer = async (playerId) => {
-
+  try {
+    const response = await fetch(
+      `${APIURL}players${playerId}`
+    );
+    const result = await response.json();
+    console.log(resultz);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const addNewPlayer = async (playerObj) => {
